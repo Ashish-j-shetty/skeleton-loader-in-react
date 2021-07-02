db9eeaf47b3c67b1688a36f86431d334c8fcf72b
AJ <ashishshetty2612@gmail.com>
2021-07-02 23:59:12 +0530

    Complete

---
+++ src/components/Article.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import SkeletonArticle from "../skeletons/SkeletonArticle";
+
+export default function Articles() {
+  const [articles, setArticles] = useState(null);
+
+  useEffect(() => {
+    setTimeout(async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+      const data = await res.json();
+
+      setArticles(data);
+    }, 4000);
+  }, []);
+
+  return (
+    <div className="articles">
+      <h2>Articles</h2>
+
+      {articles &&
+        articles.map((article) => (
+          <div className="article" key={article.id}>
+            <h3>{article.title}</h3>
+            <p>{article.body}</p>
+          </div>
+        ))}
+      {!articles &&
+        [1, 2, 3].map((n) => <SkeletonArticle key={n} theme={"light"} />)}
+    </div>
+  );
+}
---
+++ src/skeletons/SkeletonElement.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./skeleton.css";
+
+export default function SkeletonElement({ type }) {
+  const classes = `skeleton ${type}`;
+
+  return <div className={classes}></div>;
+}
